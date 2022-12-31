5d987140757e2f000bf654a53fbae6efc92ecd46
geraldaddey <geraldaddey@yahoo.com>
2022-12-31 02:31:35 +0100

    setup environment

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Finally We Stick to Mobile Development</Text>
+      <Text> Creating a new app on this shit </Text>
+      <Text>A simple NFT Marketplace Course App </Text>
       <StatusBar style="auto" />
     </View>
   );
