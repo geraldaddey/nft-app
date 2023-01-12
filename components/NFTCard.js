7719e3868c9aade33a58e0db34585994da378b37
GERALD ADDEY <geraldaddey@yahoo.com>
2023-01-12 21:56:53 +0000

    render NFT cards on home screen

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,10 +1,29 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Image, Text } from "react-native";
+import React, { Children } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { COLORS, SIZES, SHADOWS, assets } from "../constants";
 
-const NFTCard = () => {
+const NFTCard = (data) => {
   return (
-    <View>
-      <Text>NFTCard</Text>
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+      }}
+    >
+      <View
+        style={{
+          width: "100%",
+          height: 250,
+        }}
+      >
+        <Image source={data.image} />
+
+        {console.log(data)}
+      </View>
+      <Text> Gerald's NFT CARD</Text>
     </View>
   );
 };
--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,41 @@
-import React from "react";
-import { Text } from "react-native";
+import { Text, View, SafeAreaView, FlatList } from "react-native";
+import { useState } from "react";
+import { COLORS, NFTData } from "../constants";
+import { NFTCard, FocusedStatusBar, HomeHeader } from "../components";
 
 const Home = () => {
-  return <Text> Welcome to my home screen</Text>;
+  const renderItem = ({ item }) => <Item name={item.name} />;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 1 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={(item) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.secondary }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
